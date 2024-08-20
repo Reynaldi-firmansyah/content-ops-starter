aa19e4b829ab8ae64fceb8839fe919e834057efe
Reynaldi Firmansyah Caesar <reynaldiverified@gmail.com>
2024-08-21 00:08:10 +0700

    upload2
    icon launchpad

---
+++ src/components/atoms/LaunchpadLink.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LaunchpadLink = () => (
+    <a href="https://launchpad.net" target="_blank" rel="noopener noreferrer">
+        <img
+            src="/images/Canonical_Launchpad_icon_64px.png"
+            alt="Launchpad"
+            className="w-8 h-8"
+        />
+    </a>
+);
+
+export default LaunchpadLink;
